818c04b03ac9c815aa7e88636e0ef9fe3bc40aaf
Maxim Gurov <psylopunk@protonmail.com>
2022-11-10 20:33:38 +0300

    feat: first implementation of hashmaps

--- tonsdk/boc/__init__.py
+++ tonsdk/boc/__init__.py
@@ -1,7 +1,9 @@
 from ._cell import Cell, deserialize_cell_data, parse_boc_header
+from ._dict_builder import DictBuilder, begin_dict
 
 __all__ = [
     'Cell',
+    'DictBuilder', 'begin_dict',
     'deserialize_cell_data',
     'parse_boc_header',
 ]
