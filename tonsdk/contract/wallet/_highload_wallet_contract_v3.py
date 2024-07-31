23312ac4b1c1b8d2743ee15311f6e4b622c980f9
milade3013 <milade3013@gmail.com>
2024-07-31 15:23:19 +0330

    create internal tx

--- tonsdk/contract/wallet/_highload_wallet_contract_v3.py
+++ tonsdk/contract/wallet/_highload_wallet_contract_v3.py
@@ -1,9 +1,9 @@
 import decimal
 
 from .. import Contract
-from ._wallet_contract import WalletContract
+from ._wallet_contract import WalletContract, SendModeEnum
 from ...boc import Cell, begin_cell, begin_dict
-from ...utils import Address, sign_message, HighloadQueryId, check_timeout
+from ...utils import Address, sign_message, HighloadQueryId, check_timeout, to_nano
 
 
 class HighloadWalletV3Contract(WalletContract):
@@ -11,6 +11,7 @@ class HighloadWalletV3Contract(WalletContract):
         # https://github.com/ton-blockchain/highload-wallet-contract-v3
         self.code = "b5ee9c7241021001000228000114ff00f4a413f4bcf2c80b01020120020d02014803040078d020d74bc00101c060b0915be101d0d3030171b0915be0fa4030f828c705b39130e0d31f018210ae42e5a4ba9d8040d721d74cf82a01ed55fb04e030020120050a02027306070011adce76a2686b85ffc00201200809001aabb6ed44d0810122d721d70b3f0018aa3bed44d08307d721d70b1f0201200b0c001bb9a6eed44d0810162d721d70b15800e5b8bf2eda2edfb21ab09028409b0ed44d0810120d721f404f404d33fd315d1058e1bf82325a15210b99f326df82305aa0015a112b992306dde923033e2923033e25230800df40f6fa19ed021d721d70a00955f037fdb31e09130e259800df40f6fa19cd001d721d70a00937fdb31e0915be270801f6f2d48308d718d121f900ed44d0d3ffd31ff404f404d33fd315d1f82321a15220b98e12336df82324aa00a112b9926d32de58f82301de541675f910f2a106d0d31fd4d307d30cd309d33fd315d15168baf2a2515abaf2a6f8232aa15250bcf2a304f823bbf2a35304800df40f6fa199d024d721d70a00f2649130e20e01fe5309800df40f6fa18e13d05004d718d20001f264c858cf16cf8301cf168e1030c824cf40cf8384095005a1a514cf40e2f800c94039800df41704c8cbff13cb1ff40012f40012cb3f12cb15c9ed54f80f21d0d30001f265d3020171b0925f03e0fa4001d70b01c000f2a5fa4031fa0031f401fa0031fa00318060d721d300010f0020f265d2000193d431d19130e272b1fb00b585bf03"  # noqa:E501
         kwargs["code"] = Cell.one_from_boc(self.code)
+        # todo change timeout:
         kwargs["timeout"] = 60 * 11
         super().__init__(**kwargs)
         if kwargs.get("wc"):
@@ -34,14 +35,14 @@ class HighloadWalletV3Contract(WalletContract):
         return cell
 
     def create_signing_message(
-        self,
-        query_id: HighloadQueryId,
-        created_at: int,
-        send_mode: int,
-        message_to_send,
+            self,
+            query_id: HighloadQueryId,
+            created_at: int,
+            send_mode: int,
+            message_to_send,
     ):
         check_timeout(self.options["timeout"])
-
+        # todo calculate query id correctly
         cell = Cell()
         cell.bits.write_uint(self.options["wallet_id"], 32)
         cell.refs.append(message_to_send)
@@ -53,14 +54,14 @@ class HighloadWalletV3Contract(WalletContract):
         return cell
 
     def create_transfer_message(
-        self,
-        address: str,
-        amount: int,
-        query_id: HighloadQueryId,
-        create_at: int,
-        payload: str = "",
-        send_mode: int = 3,
-        need_deploy: bool = False,
+            self,
+            address: str,
+            amount: int,
+            query_id: HighloadQueryId,
+            create_at: int,
+            payload: str = "",
+            send_mode: int = SendModeEnum.ignore_errors | SendModeEnum.pay_gas_separately,
+            need_deploy: bool = False,
     ):
         if create_at is None or create_at < 0:
             raise ValueError("create_at must be number >= 0")
@@ -74,9 +75,9 @@ class HighloadWalletV3Contract(WalletContract):
         )
 
     def create_external_message(
-        self,
-        signing_message: Cell,
-        need_deploy: bool,
+            self,
+            signing_message: Cell,
+            need_deploy: bool,
     ):
         signature = sign_message(bytes(signing_message.bytes_hash()), self.options['private_key']).signature
 
@@ -111,34 +112,71 @@ class HighloadWalletV3Contract(WalletContract):
         }
 
     def create_multi_transfer_message(
-        self,
-        recipients_list: list,
-        query_id: HighloadQueryId,
-        create_at: int,
-        #     todo change send mode default here:
-        send_mode: int = 3,
-        need_deploy: bool = False
+            self,
+            recipients_list: list,
+            query_id: HighloadQueryId,
+            create_at: int,
+            #     todo change send mode default here:
+            send_mode=SendModeEnum.ignore_errors | SendModeEnum.pay_gas_separately,
+            need_deploy: bool = False
     ):
-
+        amount = 0
         if create_at is None or create_at < 0:
             raise ValueError("create_at must be number >= 0")
 
         recipients = begin_dict(16)
         for i, recipient in enumerate(recipients_list):
-            message_to_send = self.create_out_msg(
-                recipient['address'],
-                recipient['amount'],
-                recipient['payload'],
-                recipient.get('state_init'))
+            amount += recipient['amount']
+            message_to_send = self.create_internal_message_h(
+                dest=recipient['address'],
+                grams=recipient['amount'],
+                payload=recipient["payload"])
 
-            recipients.store_cell(
-                i, begin_cell() \
-                    .store_uint8(recipient.get('send_mode', 0)) \
-                    .store_ref(message_to_send).end_cell()
-            )
+            # recipients.store_cell(
+            #     i, begin_cell().store_uint8(3).store_cell(message_to_send).end_cell()
+            # )
 
+            value = Cell()
+            value.bits.write_uint8(send_mode)
+            value.write_cell(message_to_send)
+            recipients.store_cell(i, value)
+
+        # out = self.create_internal_message_h(grams=to_nano(1, "nanoton") , payload=recipients.end_cell())
         signing_message = self.create_signing_message(query_id, create_at, send_mode, recipients.end_cell())
-        # signing_message.store_maybe_ref(recipients.end_cell())
         return self.create_external_message(
             signing_message, need_deploy
         )
+
+    @classmethod
+    def create_internal_message_h(cls, dest, grams=0, payload=None):
+        src = None
+        bounce = False
+        bounced = False
+        ihr_disabled = True
+        payload_cell = Cell()
+        if payload:
+            if isinstance(payload, Cell):
+                payload_cell = payload
+            elif isinstance(payload, str):
+                if len(payload) > 0:
+                    payload_cell.bits.write_uint(0, 32)
+                    payload_cell.bits.write_string(payload)
+            else:
+                payload_cell.bits.write_bytes(payload)
+
+        message = Cell()
+        message.bits.write_bit(0)
+        message.bits.write_bit(ihr_disabled)
+        if bounce is not None:
+            message.bits.write_bit(bounce)
+        else:
+            message.bits.write_bit(Address(dest).is_bounceable)
+        message.bits.write_bit(bounced)
+        message.bits.write_address(Address(src) if src else None)
+        message.bits.write_address(Address(dest))
+        message.bits.write_grams(grams)
+        message.bits.write_uint(0, 1 + 4 + 4 + 64 + 32)
+        message.bits.write_uint(0, 1)
+        message.bits.write_uint(1, 1)
+        message.refs.append(payload_cell)
+        return message
